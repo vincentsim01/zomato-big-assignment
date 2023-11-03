aa0c85176527ddf99d9d2770b3025a138aec77f3
Vincent Kiathadi <vincentkiathadi@gmail.com>
2023-11-03 17:37:53 +0800

    Setup

---
+++ Controller/dbController.js
@@ -0,0 +1,30 @@
+let mongo = require('mongodb');
+let {MongoClient} = require('mongodb');
+let mongoUrl = "mongodb+srv://vincentkiathadi:YIfp7gktEi2USAWW@cluster0.nt2oupy.mongodb.net/?retryWrites=true&w=majority";
+let client = new MongoClient(mongoUrl);
+
+async function dbConnect(){
+    await client.connect();
+}
+
+let db = client.db('Restaurant');
+
+async function getData(colName,query){
+    let output = [];
+    try{
+        const cursor = db.collection(colName).find(query);
+        for await(const data of cursor){
+            output.push(data)
+        }
+        cursor.closed
+    }catch(err){
+        output.push({"Error":"Error in getting data"})
+    }
+    return output
+}
+
+
+module.exports = {
+    dbConnect,
+    getData
+}
